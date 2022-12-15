220e204c1cbec62401f75b2b78b9b83acc911562
William Calle <cwilliam14c@gmail.com>
2022-12-15 11:35:59 -0400

    carpeta de codigo fuente añadido

---
+++ src/com/gui/SistemaTransporteIU.java
@@ -0,0 +1,13 @@
+package com.gui;
+
+import javax.swing.JFrame;
+
+public class SistemaTransporteIU {
+	private JFrame ventanaPrincipal;
+	
+	public SistemaTransporteIU() {
+		ventanaPrincipal = new JFrame("Sistema Transporte - Trans Bolivia");
+		ventanaPrincipal.setSize(650, 430);
+		ventanaPrincipal.setVisible(true);
+	}
+}
